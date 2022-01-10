278569a7be4fdd0eb8043a44d7cfc67344a4dd1d
Jason <beefers331@gmail.com>
2022-01-10 15:07:29 -0700

    Added chain basis

--- src/slicerator.nim
+++ src/slicerator.nim
@@ -1,4 +1,4 @@
-import std/[macros, enumerate, macrocache, typetraits, genasts, strformat]
+import std/[macros, enumerate, macrocache, typetraits, genasts, strformat, strutils]
 import closures
 export closures
 
@@ -271,6 +271,107 @@ macro filter*(forLoop: ForLoopStmt): untyped =
         if expr:
           body
 
+type
+  ChainableOps = enum
+    coFilter = "filter"
+    coMap = "map"
+  ChainOp = object
+    kind: ChainableOps
+    val: NimNode ## Only used for deepest
+    op: NimNode
+
+proc getOps(call: NimNode): seq[ChainOp] =
+  var n = call[1]
+  while n.kind == nnkCall:
+    try:
+      let opKind = parseEnum[ChainableOps](n[0][^1].strVal)
+      result.add ChainOp(kind: opKind, op: n[^1])
+      if n[0][0].kind == nnkIdent:
+        result[^1].val = n[0][0]
+      n = n[0][0]
+    except:
+      error(fmt"Invalid operation {call[0][^1]}.", call[0][^1])
+
+proc addOp(n, fieldName: NimNode, chainOp: ChainOp) =
+  let op = chainOp.op
+
+  if n[^1].kind == nnkLetSection:
+    n.add:
+      case chainOp.kind:
+      of coMap:
+        genAst(n, fieldName, op):
+          block:
+            let fieldName = op
+      of coFilter:
+        genAst(n, fieldName, op):
+          if op: discard
+  elif n[^1].kind == nnkDiscardStmt:
+    n[^1] =
+      case chainOp.kind:
+      of coMap:
+        genAst(n, fieldName, op):
+          block:
+            let fieldName = op
+      of coFilter:
+        genAst(n, fieldName, op):
+          if op: discard
+  else:
+    n[^1].addOp(fieldName, chainOp)
+
+proc addBody(n, body: NimNode) =
+  if n[^1].kind == nnkLetSection:
+    n.add body
+  elif n[^1].kind == nnkIfStmt and n[^1][^1].kind == nnkDiscardStmt:
+    n[^1][^1] = body
+  else:
+    n[^1].addBody(body)
+
+macro chain(forloop: ForLoopStmt): untyped =
+  let passedVars = block:
+    var val  = 0
+    for n in forLoop:
+      if n.kind != nnkIdent:
+        break
+      inc val
+    val
+
+  if passedVars > 2:
+    error("Invalid variable count passed to 'chain'.", forLoop[2])
+  let
+    ops = forloop[^2].getOps
+    varName =
+      if passedVars == 2:
+        forLoop[1]
+      else:
+        forLoop[0]
+    body = forLoop[^1]
+  for x in ops.revitems:
+    if x.val != nil:
+      case x.kind:
+      of coMap:
+        result = genAst(iter = x.val, varName, op = x.op):
+          for varName in iter:
+            let varName = op
+      of coFilter:
+        result = genAst(iter = x.val, varName, op = x.op):
+          for varName in iter:
+            if op: discard
+
+    else:
+      result.addOp(varName, x)
+
+  result.addBody(body)
+  if passedVars == 2:
+    result[^1].add newCall("inc", forLoop[0])
+    result = nnkBlockStmt.newTree(newEmptyNode(), newStmtList(newVarStmt(forLoop[0], newLit(0)), result))
+    
+  echo result.repr
+
+
+var val = 0
+
+for x in chain lines("input.txt").map(parseInt(x)).filter(x + y + z < 3):
+  inc val
 
 
 macro zipIter*(forBody: ForLoopStmt): untyped =
