eb1211fbd8c7080f9428ba4fbc5ec3bfb531e28f
Jason <beefers331@gmail.com>
2022-01-18 23:53:16 -0700

    Added unpack checking logic for chain

--- src/chainimpl.nim
+++ src/chainimpl.nim
@@ -12,6 +12,19 @@ type
     kind: ChainableOps
     val: NimNode ## Only used for deepest
 
+macro unpackCheck(left: untyped, right: typed): untyped =
+  ## Internal macro for emitting helpful errors on unpacking attempts
+  let rightImpl = right.getType
+  if rightImpl.len == 0 or not rightImpl[0].eqIdent"tuple":
+    error(fmt"Can only unpack 'tuples': '{right.repr}' is '{rightImpl.repr}'", right)
+  elif left.len != rightImpl.len - 1:
+    error(fmt"Expected {right.getType.len - 1} variables to unpack into, but got {left.len}.", left[0])
+  else:
+    result = nnkLetSection.newTree(nnkVarTuple.newNimNode)
+    result[0].add left[0..^1]
+    result[0].add newEmptyNode()
+    result[0].add right
+
 proc getOps(call: NimNode): seq[ChainOp] =
   var n = call[1]
   while n.kind in {nnkCall, nnkDotExpr}:
@@ -51,12 +64,10 @@ proc addOp(n, fieldName: NimNode, chainOp: ChainOp) =
       genAst(n, fieldName, op):
         if op: discard
     of coUnpack:
-      var varTup = nnkVarTuple.newTree(op[0..^1])
-      varTup.add newEmptyNode()
-      varTup.add fieldName
-      nnkLetSection.newTree(varTup)
+      var varTup = nnkTupleConstr.newTree(op[0..^1])
+      newCall(bindSym"unpackCheck", varTup, fieldName)
 
-  if n[^1].kind == nnkLetSection:
+  if n[^1].kind in {nnkLetSection, nnkCall}:
     n.add addOpImpl()
   elif n[^1].kind == nnkDiscardStmt:
     n[^1] = addOpImpl()
@@ -117,18 +128,16 @@ macro chain*(forloop: ForLoopStmt): untyped =
       for varName in iter:
         if op: discard
   of coUnpack:
-    var varTup = nnkVarTuple.newTree(lastOp.op[0..^1])
-    varTup.add newEmptyNode()
-    varTup.add varName
-    varTup = nnkLetSection.newTree(varTup)
+    var varTup = nnkTupleConstr.newTree(lastOp.op[0..^1])
+    varTup = newCall(bindSym"unpackCheck", varTup, varName)
     result = genAst(iter = iter.val, varName, varTup):
       for varName in iter:
         varTup
 
   while ops.len > 0: # Add remaining ops
     result.addOp(varName, ops.pop)
-
   result.addBody(body)
   if passedVars == 2: # We have an index variable, so emit calls for it
     result[^1].add newCall("inc", forLoop[0])
     result = nnkBlockStmt.newTree(newEmptyNode(), newStmtList(newVarStmt(forLoop[0], newLit(0)), result))
+
