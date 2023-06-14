47e0b7e366541efb764ef32c5b08a5226cf01185
Zoom <zoomrmc+git@gmail.com>
2023-06-14 06:27:07 +0500

    More adaptors, more consumers

--- src/itermacros.nim
+++ src/itermacros.nim
@@ -1,4 +1,4 @@
-import std/[macros, genasts]
+import std/[macros, genasts, options]
 
 macro genIter*[T](iter: iterable[T], body: varargs[untyped]): untyped =
   if body.len == 0:
@@ -42,37 +42,46 @@ macro genIter*[T](iter: iterable[T], body: varargs[untyped]): untyped =
   iterInvoke[0] = decl[0]
   result = newStmtList(decl, iterInvoke)
 
-template map*[T; Y](iter: iterable[T], fn: proc(x: T): Y): untyped =
+#--------------------------------------------------------------------------
+# Adaptors
+#--------------------------------------------------------------------------
+
+template map*[T; Y](iter: iterable[T]; fn: proc(x: T): Y): untyped =
   genIter(iter):
     yield fn(it)
 
-template mapIt*[T](iter: iterable[T], expr: untyped): untyped =
+template map*[T; Y](iter: iterable[T]; fn: proc(x: T): Y {.inline.}): untyped =
+  # Overloard for inline procs
+  genIter(iter):
+    yield fn(it)
+
+template mapIt*[T](iter: iterable[T]; expr: untyped): untyped =
   genIter(iter):
     yield expr
 
-template filter*[T](iter: iterable[T], fn: proc(x: T): bool): untyped =
+template filter*[T](iter: iterable[T]; pred: proc(x: T): bool): untyped =
   genIter(iter):
-    if fn(it):
+    if pred(it):
       yield it
 
-template filterIt*[T](iter: iterable[T], expr: untyped): untyped =
+template filterIt*[T](iter: iterable[T]; expr: untyped): untyped =
   genIter(iter):
     if expr:
       yield it
 
-macro genTuple*(typ: untyped, amount: static int): untyped =
+macro genTuple*(typ: untyped; amount: static int): untyped =
   result = nnkPar.newTree()
   for _ in 0..<amount:
     result.add typ
 
-proc set(t: var tuple, ind: int, val: auto) =
+proc set(t: var tuple; ind: int; val: auto) =
   var i = 0
   for field in t.fields:
     if i == ind:
       field = val
     inc i
 
-template group*[T](iter: iterable[T], amount: static int): untyped =
+template group*[T](iter: iterable[T]; amount: static Positive): untyped =
   genIter(iter):
     var
       val: genTuple(T, amount)
@@ -83,30 +92,52 @@ template group*[T](iter: iterable[T], amount: static int): untyped =
       yield val
     inc ind
 
-template skip*[T](iter: iterable[T], amount: int): untyped =
+template skip*[T](iter: iterable[T]; amount: Natural): untyped =
   genIter(iter):
-    var counter = amount
+    var counter = 0
   do:
-    if counter > 0:
-      dec counter
-      continue
+    if counter >= amount:
+      yield it
+    inc counter
+
+template skipWhileIt*[T](iter: iterable[T]; expr: untyped): untyped =
+  genIter(iter):
+    var skipping = true
+  do:
+    if skipping:
+      skipping = expr
+    else:
+      yield it
+
+template skipWhile*[T](iter: iterable[T]; pred: proc(x: T): bool): untyped=
+  skipWhileIt(iter, pred(it))
+
+template take*[T](iter: iterable[T]; amount: Natural): untyped =
+  genIter(iter):
+    var counter = 0
+  do:
+    if counter >= amount:
+      break
+    yield it
+    inc counter
+
+template takeWhileIt*[T](iter: iterable[T]; expr: untyped): untyped =
+  genIter(iter):
+    if not expr:
+      break
     yield it
 
-template take*[T](iter: iterable[T], amount: int): untyped =
-  genIter(iter):
-    var counter = amount - 1
-  do:
-    if counter > 0:
-      yield it
-      dec counter
-    else:
-      break
+template takeWhile*[T](iter: iterable[T]; pred: proc(x: T): bool): untyped=
+  takeWhileIt(iter, pred(it))
 
-template collect*[T](iter: iterable[T], size = 0): seq[T] =
-  var val = newSeqOfCap[T](size)
-  for x in iter:
-    val.add x
-  val
+template stepBy*[T](iter: iterable[T]; step: Positive): untyped =
+  genIter(iter):
+    var count = step
+  do:
+    if count == step:
+      yield it
+      count = 0
+    inc count
 
 template enumerate*[T](iter: iterable[T]): untyped =
   genIter(iter):
@@ -114,3 +145,122 @@ template enumerate*[T](iter: iterable[T]): untyped =
   do:
     yield (count, it)
     inc count
+
+
+#--------------------------------------------------------------------------
+# Consumers
+#--------------------------------------------------------------------------
+
+type
+  Comparable* = concept a
+    (a < a) is bool
+  Summable* = concept a, type T # Additive?
+    (a + a) is T
+  Multipliable* = concept a, type T # Multiplicative?
+    (a * a) is T
+
+template collect*[T](iter: iterable[T]; size: Natural = 0): seq[T] =
+  var val = newSeqOfCap[T](size)
+  for x in iter:
+    val.add x
+  val
+
+template collect*[T, U](iter: iterable[T]; t: typeDesc[U]): U =
+  # TODO? Replace `when compiles` with specialization on concepts
+  var acc = default(t)
+  for x in iter:
+    when compiles(acc.add(default(typeOf(T)))):
+      acc.add x
+    elif compiles(acc.incl(default(typeOf(T)))):
+      acc.incl x
+    elif compiles(acc.push(default(typeOf(T)))):
+      acc.push x
+  acc
+
+template fold*[T, U](iter: iterable[T]; init: U; fn: proc(acc: sink U; it: T): U): U =
+  var acc: U = init
+  for it in iter:
+    acc = fn(acc, it)
+  acc
+
+template fold*[T, U](iter: iterable[T]; init: U; fn: proc(acc: var U; it: T)): U =
+  var acc: U = init
+  for it in iter:
+    fn(acc, it)
+  acc
+
+template foldIt*[T, U](iter: iterable[T]; init: U; expr: untyped): U =
+  var acc {.inject.}: U = init
+  for it {.inject.} in iter:
+    acc = expr
+  acc
+
+
+template selectByCmp[T: Comparable](iter: iterable[T]; expr: untyped): T =
+  var
+    requiresInit = true
+    acc {.inject.}: T
+  for it {.inject.} in iter:
+    if unlikely(requiresInit):
+      acc = it
+      requiresInit = false
+    if expr:
+      acc = it
+  acc
+
+template min*[T: Comparable](iter: iterable[T]): T =
+  selectByCmp(iter, it < acc)
+
+template max*[T: Comparable](iter: iterable[T]): T =
+  selectByCmp(iter, acc < it)
+
+template count*[T](iter: iterable[T]): Natural =
+  iter.foldIt(0, acc+1)
+
+template sum*[T: Summable](iter: iterable[T]): T =
+  iter.foldIt(default(typeOf(T)), acc + it)
+
+template product*[T: Multipliable](iter: iterable[T]): T =
+  var
+    requiresInit = true
+    acc: T
+  for it in iter:
+    if unlikely(requiresInit):
+      acc = it
+      requiresInit = false
+      continue
+    acc = acc * it
+  acc
+
+template anyIt*[T](iter: iterable[T]; expr: untyped): bool =
+  var result = false
+  for it {.inject.} in iter:
+    if expr:
+      result = true
+      break
+  result
+
+template any*[T](iter: iterable[T]; pred: proc(x: T): bool): bool =
+  anyIt(iter, pred(it))
+
+template allIt*[T](iter: iterable[T]; expr: untyped): bool =
+  var result = true
+  for it {.inject.} in iter:
+    if not expr:
+      result = false
+      break
+  result
+
+template all*[T](iter: iterable[T]; pred: proc(x: T): bool): bool =
+  allIt(iter, pred(it))
+
+template findIt*[T](iter: iterable[T]; expr: untyped): Option[T] =
+  var result = none(typeOf(T))
+  for it {.inject.} in iter:
+    if expr:
+      result = some(it)
+      break
+  result
+
+template find*[T](iter: iterable[T]; pred: proc(x: T): bool): Option[T] =
+  findIt(iter, pred(it))
--- tests/titermacros.nim
+++ tests/titermacros.nim
@@ -1,8 +1,12 @@
 import itermacros, closures
 import std/unittest
 
-var theVal = [10, 20, 30, 40]
 suite "Iterator macros":
+  const theVal = [10, 20, 30, 40]
+  test "Basic collect":
+    # This is used extensively for other tests, so checking it first
+    check theVal.items.collect() == @theVal
+
   test "Basic tests":
     for (i, x) in theVal.items.mapIt(it * 2).group(2).enumerate():
       case i
@@ -17,9 +21,133 @@ suite "Iterator macros":
 
     for x in theVal.items.skip(2).group(2):
       check x == (30, 40)
+
   test "Closure tests":
     let myClos1 = asClosure theVal.items.skip(2).group(2)
     check myClos1() == (30, 40)
     let myClos2 = asClosure theVal.items.group(2).mapIt($it)
     check myClos2() == "(10, 20)"
   check theVal.items.group(2).mapIt($it).collect == ["(10, 20)", "(30, 40)"]
+
+
+import std/[options, tables, sets]
+from std/strutils import isUpperAscii, isLowerAscii, toUpperAscii, split
+
+const
+  ints = [-2, -1, 1, 3, -4, 5]
+  chars = ['a', '.', 'b', 'C', 'z', 'd']
+  strs = ["foo", "BAR", "Niklaus", "deadbeef"]
+  text = "Epicurus sought tranquility through simplicity and pleasure."
+
+suite "Iterator adaptors":
+  test "map":
+    # abs is {.inline.}
+    check ints.items.map(proc(x:int):int = abs(x)).collect() == @[2, 1, 1, 3, 4, 5]
+    check chars.items.map(toUpperAscii).collect() == @['A', '.', 'B', 'C', 'Z', 'D']
+    check strs.items.map(toUpperAscii).collect() == @["FOO", "BAR", "NIKLAUS", "DEADBEEF"]
+
+    check ints.items.mapIt(abs(it)).collect() == @[2, 1, 1, 3, 4, 5]
+    check chars.items.mapIt(char(it.ord + 1)).collect() == @['b', '/', 'c', 'D', '{', 'e']
+    check strs.items.mapIt((var s = it; s.setLen(1); s)).collect() == @["f", "B", "N", "d"]
+
+  test "filter":
+    check ints.items.filter(proc(x:int):bool = x > 0).collect() == @[1, 3, 5]
+    check chars.items.filter(proc(x:char):bool = x in {'a'..'z'}).collect() == @['a', 'b', 'z', 'd']
+    check strs.items.filter(proc(x:string):bool = x.len == 3).collect() == @["foo", "BAR"]
+
+    check ints.items.filterIt(it mod 2 == 0).collect() == @[-2, -4]
+    check chars.items.filterIt(it notin {'a'..'d'}).collect() == @['.', 'C', 'z']
+    check strs.items.filterIt(it.len > 7).collect() == @["deadbeef"]
+
+  test "genTuple":
+    check ints.items.group(2).collect() == @[(-2, -1), (1, 3), (-4, 5)]
+    check chars.items.group(6).collect() == @[('a', '.', 'b', 'C', 'z', 'd')]
+
+  test "skip":
+    check ints.items.skip(3).collect() == @[3, -4, 5]
+    check chars.items.skip(6).collect() == newSeq[char](0)
+    check strs.items.skip(0).collect() == @strs
+
+  test "skipWhile":
+    check ints.items.skipWhile(proc(x:int):bool = x < 0).collect() == @[3, -4, 5]
+    check ints.items.skipWhileIt(it < 0).collect() == @[3, -4, 5]
+
+  test"take":
+    check ints.items.take(3).collect() == @[-2, -1, 1]
+    # more than items in the iterator
+    check chars.items.take(6).collect() == @chars
+    # take zero items
+    check strs.items.take(0).collect() == newSeq[string](0)
+
+  test "takeWhile":
+    check ints.items.takeWhile(proc(x:int):bool = x < 0).collect() == @[-2, -1]
+    check ints.items.takeWhileIt(it < 0).collect() == @[-2, -1]
+
+  test "stepBy":
+    check ints.items.stepBy(2).collect() == @[-2, 1, -4]
+    check text.items.stepBy(5).foldIt("", (acc.add(it); acc)) == "Ero qtr ly s"
+    # first element is always returned
+    check chars.items.stepBy(9000).collect() == @['a']
+
+  test "enumerate":
+    check ints.items.enumerate.collect() == @[(0, -2), (1, -1), (2, 1), (3, 3), (4, -4), (5, 5)]
+
+suite "Iterator consumers":
+
+  test "min-max":
+    check ints.items.min() == -4
+    check chars.items.min() == '.'
+    check strs.items.min() == "BAR"
+
+    check ints.items.max() == 5
+    check chars.items.max() == 'z'
+    check strs.items.max() == "foo"
+
+  test "count":
+    check ints.items.count() == 6
+    check chars.items.count() == 6
+    check strs.items.count() == 4
+
+  test "sum":
+    check ints.items.sum() == 2
+
+  test "product":
+    check ints.items.product() == -120
+
+  test "any-all":
+    check ints.items.anyIt(it > 1) == true
+    check chars.items.anyIt(it.isUpperAscii)
+    check chars.items.any(isLowerAscii)
+    check chars.items.allIt(it in  {'.', 'C', 'a'..'z'})
+    check chars.items.all(isLowerAscii) == false
+
+  test "find":
+    check ints.items.find(proc(x:int):bool = x > 1) == some(3)
+    check ints.items.findIt(it < -2) == some(-4)
+    check strs.items.find(proc(x:string):bool = x.items.all(isUpperAscii)) == some("BAR")
+    check strs.items.findIt(it == "Dijkstra").isNone()
+    check chars.items.find(proc(x:char):bool = x.ord > 'y'.ord) == some('z')
+
+  test "fold":
+    func appended(acc: sink seq[string]; it:int): seq[string] =
+      result = acc
+      result.add($it)
+    proc grow(acc: var seq[string]; it:int) =
+      acc.add($it)
+
+    check ints.items.fold(@["acc"], appended) == @["acc", "-2", "-1", "1", "3", "-4", "5"]
+    check ints.items.fold(@["acc"], grow) == @["acc", "-2", "-1", "1", "3", "-4", "5"]
+    check chars.items.foldIt({'@'}, (acc.incl(it); acc)) == {'.', '@', 'C', 'a', 'b', 'd', 'z'}
+    let t = chars.items.enumerate.foldIt(initTable[char, int](), (acc[it[1]]=it[0]; acc))
+    check t['d'] == 5
+
+  test "collect to seq":
+    check ints.items.collect() == @ints
+    check chars.items.collect() == @chars
+    check strs.items.collect() == @strs
+
+  test "collect to specific containers":
+    check text.items.collect(set[char]) == {' ', '.', 'E', 'a', 'c', 'd', 'e', 'g', 'h', 'i', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'y'}
+    check ints.items.collect(seq[int]) == @[-2, -1, 1, 3, -4, 5]
+    check strs.items.collect(HashSet[string]) == toHashSet(strs)
+    check chars.items.collect(string) == "a.bCzd"
